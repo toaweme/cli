dacf14fb2a8c07916cafa365be9237bf0f6623f0
Ignas Bernotas <ignas@iber.lt>
2026-05-27 22:45:22 +0300

    fix: interface

--- app.go
+++ app.go
@@ -21,6 +21,7 @@ const helpCommand = "help"
 type App interface {
 	Commands() []Command[any]
 	Settings() Settings
+	Default(cmd Command[any]) Command[any]
 	Add(name string, cmd Command[any]) Command[any]
 	Run(osArgs []string) error
 }
